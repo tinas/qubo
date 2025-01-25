ba2d0e7b22527321e7d75f60da3d6df3d0751bac
tinas <ahmettinastepe0@gmail.com>
2025-01-25 12:28:28 +0300

    test: add jest configuration

---
+++ jest.config.js
@@ -0,0 +1,26 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/lib'],
+  testMatch: ['**/__tests__/**/*.test.ts'],
+  collectCoverageFrom: ['lib/**/*.ts', '!lib/**/*.d.ts'],
+  coverageDirectory: 'coverage',
+  coverageReporters: ['text', 'lcov'],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80
+    }
+  },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json'
+      }
+    ]
+  }
+}; 
